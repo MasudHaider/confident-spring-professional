e21c58114f7197d628988446000e4885ec2f6e5f
Masud Haider <masud6563@gmail.com>
2025-12-25 11:17:25 +0200

    initial commit

---
+++ src/main/java/com/haider/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java
@@ -0,0 +1,56 @@
+package com.haider.myfancypdfinvoices.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.haider.myfancypdfinvoices.service.InvoiceService;
+import com.haider.myfancypdfinvoices.model.Invoice;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MyFancyPdfInvoicesServlet extends HttpServlet {
+    private InvoiceService invoiceService = new InvoiceService();
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getRequestURI().equalsIgnoreCase("/")) {
+
+            response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().print(
+                    "<html>\n" +
+                    "<body>\n" +
+                    "<h1>Hello World</h1>\n" +
+                    "<p>This is my very first, embedded Tomcat, HTML Page!</p>\n" +
+                    "</body>\n" +
+                    "</html>");
+        }
+        else if (request.getRequestURI().equalsIgnoreCase("/invoices")){
+            response.setContentType("application/json; charset=UTF-8");
+            List<Invoice> invoices = invoiceService.findAll();
+            response.getWriter().print(objectMapper.writeValueAsString(invoices));
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+
+            String userId = request.getParameter("user_id");
+            Integer amount = Integer.valueOf(request.getParameter("amount"));
+
+            Invoice invoice = invoiceService.create(userId, amount);
+
+            response.setContentType("application/json; charset=UTF-8");
+
+            String json = new ObjectMapper().writeValueAsString(invoice);
+
+            response.getWriter().print(json);
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+    }
+}
